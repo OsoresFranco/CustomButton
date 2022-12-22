10e55fc680c6d158caf4947a94595f94f5fe5854
Franco Osores <77215052+filburt88@users.noreply.github.com>
2022-12-22 09:50:45 -0300

    Initial commit

---
+++ CustomButtonToTry/src/main/java/com/example/custombuttontotry/CustomButtonTry.kt
@@ -0,0 +1,28 @@
+package com.example.custombuttontotry
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomButton() {
+    Button(onClick = { /*TODO*/ }, modifier = Modifier.background(color = Color.Green)) {
+
+    }
+}
